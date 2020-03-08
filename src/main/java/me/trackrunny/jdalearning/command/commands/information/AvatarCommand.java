7b28eec15ef4ba31a259325092f99cdb491fbe62
TrackRunny <trackrunny@protonmail.com>
2020-03-07 20:17:56 -0800

    Added avatar command

---
+++ src/main/java/me/trackrunny/jdalearning/command/commands/information/AvatarCommand.java
@@ -0,0 +1,73 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * JDALearningBot - Discord bot to learn JDA 4 library                       *
+ * Copyright (C) 2019-2020 TrackRunny                                        *
+ *                                                                           *
+ * This program is free software: you can redistribute it and/or modify      *
+ * it under the terms of the GNU General Public License as published by      *
+ * the Free Software Foundation, either version 3 of the License, or         *
+ * (at your option) any later version.                                       *
+ *                                                                           *
+ * This program is distributed in the hope that it will be useful,           *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the             *
+ * GNU General Public License for more details.                              *
+ *                                                                           *
+ * You should have received a copy of the GNU General Public License         *
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+package me.trackrunny.jdalearning.command.commands.information;
+
+import me.trackrunny.jdalearning.command.CommandContext;
+import me.trackrunny.jdalearning.command.ICommand;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import java.awt.*;
+import java.util.List;
+
+public class AvatarCommand implements ICommand {
+    public void handle(CommandContext ctx) {
+        final List<String> args = ctx.getArgs();
+        final TextChannel channel = ctx.getChannel();
+        final Message message = ctx.getMessage();
+
+        final EmbedBuilder embedBuilder = new EmbedBuilder();
+
+        if (args.isEmpty() || message.getMentionedMembers().isEmpty()) {
+            embedBuilder.setTitle("Missing Arguments");
+            embedBuilder.setDescription("Usage: `j!avatar <@user>`");
+            embedBuilder.setFooter("LinuxBoi | Rewrote in JDA");
+            embedBuilder.setColor(new Color(241, 90, 36));
+            channel.sendMessage(embedBuilder.build()).queue();
+            return;
+        }
+
+        final Member target = message.getMentionedMembers().get(0);
+        final String member_name = target.getUser().getAsTag();
+        final String member_avatar = target.getUser().getEffectiveAvatarUrl() + "?size=1024";
+
+        embedBuilder.setTitle(String.format("%s's Avatar", member_name));
+        embedBuilder.setImage(member_avatar);
+        embedBuilder.setFooter("LinuxBoi | Rewrote in JDA");
+        embedBuilder.setColor(new Color(241, 90, 36));
+
+        channel.sendMessage(embedBuilder.build()).queue();
+    }
+
+    public String getName() {
+        return "avatar";
+    }
+
+    public MessageEmbed getHelp() {
+        final EmbedBuilder embedBuilder = new EmbedBuilder();
+        embedBuilder.setTitle("Command Usage");
+        embedBuilder.setDescription("Gets a user's avatar profile picture.\nUsage: `j!avatar <@user>`");
+        embedBuilder.setFooter("LinuxBoi | Rewrote in JDA");
+        embedBuilder.setColor(new Color(241, 90, 36));
+        return embedBuilder.build();
+    }
+}
